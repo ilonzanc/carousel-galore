98bfcd3ef400db539355ad76721c224115fdecb3
ilonzanc <zancaneri@gmail.com>
2019-01-11 15:19:02 +0100

    [WIP] Add carousel with bullets

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,24 +1,26 @@
 (function(){
   //-- Config
-  let firstCarousel = document.querySelector('.first-carousel-block');
-  /* let secondCarousel = document.querySelector('.second-carousel-block');
-  let thirdCarousel = document.querySelector('.third-carousel-block');
-  let fourthCarousel = document.querySelector('.fourth-carousel-block'); */
+  //-- 1. Select a carousel block
+  //-- 2. Set window.itemIndex = 0;
+  //-- 3. Initiate window.slideInterval if automatic carousel
+  //-- 4. initCarousel(carousel, factorNumber)
 
-  window.firstItemIndex = 0;
-  /* window.secondItemIndex = 0;
-  window.thirdItemIndex = 0;
-  window.fourthItemIndex = 0; */
+  let fifthCarousel = document.querySelector('.fifth-carousel-block');
 
-  /* window.thirdSlideInterval;
-  window.fourthSlideInterval; */
+  window.fifthItemIndex = 0;
 
-  initCarousel(firstCarousel, 1, window.firstItemIndex, 'default', false);
-  /* initCarousel(secondCarousel, 1, window.secondItemIndex, 'infinite', false);
-  initCarousel(thirdCarousel, 1, window.thirdItemIndex, 'default', true, window.thirdSlideInterval);
-  initCarousel(fourthCarousel, 1, window.fourthItemIndex, 'infinite', true, window.fourthSlideInterval); */
+  window.fifthSlideInterval;
+
+  initCarousel(fifthCarousel, 1, window.fifthItemIndex, 'infinite', true, window.fifthSlideInterval);
 
   function initCarousel(carouselBlock, factorNumber, index, carouselType, autoSlide, interval = null) {
+    // carouselBlok: the carousel you want to initiate
+    // factorNumber: how many items you want to be visible per slide
+    // index: the window.itemIndex
+    // carouselType: 'default' or 'infinite'
+    // autoSlide: true or false
+    // interval: window.slideInterval if there is one. Defaults to null
+
     let carouselContainer = carouselBlock.querySelector('.carousel-container');
     let carousel = carouselContainer.querySelector('.carousel');
 
@@ -32,16 +34,26 @@
     setCarouselWidth(carouselItems, carousel);
     let carouselSlideWidth = setCarouselSlideWidth(carouselItems);
 
+
+
     if (carouselItemsCount > 1) {
       carouselLeft.addEventListener('click', function(e) {
         e.preventDefault();
 
-        index--
-
         if (carouselType == 'default') {
-          index = shiftSlide(1, carouselItems, carousel, carouselSlideWidth, index);
+          if (interval) {
+            slideCarousel(carouselType, interval, 1);
+          } else {
+            index = shiftSlide(1, carouselItems, carousel, carouselSlideWidth, index);
+          }
+
         } else if (carouselType == 'infinite') {
-          index = infiniteShiftSlide(1, carouselItems, carousel, carouselSlideWidth, index);
+          if (interval) {
+            slideCarousel(carouselType, interval, 1);
+          } else {
+            index = infiniteShiftSlide(1, carouselItems, carousel, carouselSlideWidth, index);
+          }
+
         }
 
       });
@@ -49,14 +61,23 @@
       carouselRight.addEventListener('click', function(e) {
         e.preventDefault();
 
-        index++
-
         if (carouselType == 'default') {
-          index = shiftSlide(-1, carouselItems, carousel, carouselSlideWidth, index);
+          if (interval) {
+            slideCarousel(carouselType, interval, -1);
+          } else {
+            index = shiftSlide(-1, carouselItems, carousel, carouselSlideWidth, index);
+          }
         } else if (carouselType == 'infinite') {
-          index = infiniteShiftSlide(-1, carouselItems, carousel, carouselSlideWidth, index);
+          if (interval) {
+            slideCarousel(carouselType, interval, -1);
+          } else {
+            index = infiniteShiftSlide(-1, carouselItems, carousel, carouselSlideWidth, index);
+          }
+
         }
       });
+
+
 
       if (window.innerWidth < 700) {
         factorNumber = 1;
@@ -66,32 +87,58 @@
         factorNumber = 2;
       }
 
-
-
       if (autoSlide == true) {
-        interval = window.setTimeout(slideCarousel, 2000, carouselType, interval);
+        interval = window.setTimeout(slideCarousel, 5000, carouselType, interval, -1);
       }
+
+
+
+      if (carousel.dataset.bullets == "true") {
+        let bullets = carouselBlock.querySelectorAll('.carousel-bullets span');
+        for (let b = 0; b < bullets.length; b++) {
+          bullets[b].onclick = e => {
+            bullets[b].classList.remove('active');
+
+            bullets.forEach(bullet => {
+              bullet.classList.remove('active');
+            })
+
+            e.target.classList.add('active');
+
+            console.log('index: ' + index);
+            console.log('selected index: ' + e.target.dataset.carouselIndex);
+            direction = index - parseInt(e.target.dataset.carouselIndex);
+            console.log(direction);
+            //index = e.target.dataset.carouselIndex;
+            slideBulletsCarousel(carouselType, interval, direction);
+          }
+        }
+      }
+
 
 
       window.addEventListener('resize', handleWindowResize);
 
-      function slideCarousel(type, interval) {
-        console.log(type);
 
+
+      function slideCarousel(type, interval, direction) {
         if (type == 'default') {
-          index++;
-          index = shiftSlide(-1, carouselItems, carousel, carouselSlideWidth, index);
+          index = shiftSlide(direction, carouselItems, carousel, carouselSlideWidth, index);
         } else if (type == 'infinite') {
-          index--;
-          index = infiniteShiftSlide(1, carouselItems, carousel, carouselSlideWidth, index);
+          index = infiniteShiftSlide(direction, carouselItems, carousel, carouselSlideWidth, index);
         }
 
         window.clearTimeout(interval);
-        interval = window.setTimeout(slideCarousel, 2000, type, interval);
+        interval = window.setTimeout(slideCarousel, 5000, type, interval, -1);
+      }
+
+      function slideBulletsCarousel(type, interval, direction) {
+        index = shiftBulletsSlide(direction, carouselItems, carousel, carouselSlideWidth, index);
+        console.log('index after slide: ' + index);
+        window.clearTimeout(interval);
+        interval = window.setTimeout(slideCarousel, 5000, type, interval, -1);
       }
     }
-
-
   }
 
   function handleWindowResize() {
@@ -129,20 +176,17 @@
       el.classList.remove("active");
     });
     if (direction === 1) {
-      console.log('we go left');
-      console.log(index);
+      index--;
       if (index >= 0) {
         currentPosition = -(direction * slideWidth) * (index);
-        console.log('prev');
       } else if (index == -1) {
-        console.log('last');
         index = items.length - 1;
 
         currentPosition = -(direction * slideWidth) * (index);
       }
 
     } else if (direction === -1) {
-      console.log('we go right');
+      index++;
       if (index < items.length) {
         currentPosition = (direction * slideWidth) * index;
       } else if (index == items.length) {
@@ -162,13 +206,20 @@
     return index;
   }
 
+
+
   function infiniteShiftSlide(direction, items, carousel, slideWidth, index) {
+
     [].forEach.call(items, el => {
       el.classList.remove("active");
     });
 
+    [].forEach.call(document.querySelectorAll('.carousel-bullets span'), bullet => {
+      bullet.classList.remove("active");
+    });
+
     if (direction === 1) {
-      console.log('left');
+      index--;
 
       let lastItem = carousel.querySelector('.carousel-item:last-child');
       lastItem.parentNode.prepend(lastItem);
@@ -187,7 +238,7 @@
       carousel.querySelector('.carousel-item:nth-child(1)').classList.add('active');
 
     } else if (direction === -1) {
-      console.log('right');
+      index++;
 
       carousel.classList.add('transition');
       carousel.style.transform = 'translateX(' + (direction * slideWidth) + 'px)';
@@ -202,6 +253,62 @@
 
       carousel.querySelector('.carousel-item:nth-child(2)').classList.add('active');
     }
+    if (index == items.length) {
+      index = 0;
+    }
+    console.log('.carousel-bullets span:nth-child(' + (index + 1) +')');
+    if (document.querySelector('.carousel-bullets span:nth-child(' + (index + 1) +')')) {
+      document.querySelector('.carousel-bullets span:nth-child(' + (index + 1) +')').classList.add('active');
+    } else {
+      document.querySelector('.carousel-bullets span:nth-child(1)').classList.add('active');
+    }
+    console.log(index);
+    return index;
+  }
+
+  function shiftBulletsSlide(direction, items, carousel, slideWidth, index) {
+    [].forEach.call(items, el => {
+      el.classList.remove("active");
+    });
+
+    index = index - direction;
+    if (direction > 0) {
+      for (let i = 0; i < direction; i++) {
+        let lastItem = carousel.querySelector('.carousel-item:last-child');
+        lastItem.parentNode.prepend(lastItem);
+      }
+
+      carousel.style.transform = 'translateX(' + (-direction * slideWidth) + 'px)';
+
+      setTimeout(function(){
+        carousel.classList.add('transition');
+        carousel.style.transform = 'translateX( 0px)';
+      }, 100);
+
+      setTimeout(function(){
+        carousel.classList.remove('transition');
+      }, 800);
+
+      carousel.querySelector('.carousel-item:nth-child(1)').classList.add('active');
+
+    } else if (direction < -1) {
+
+      carousel.classList.add('transition');
+      carousel.style.transform = 'translateX(' + (direction * slideWidth) + 'px)';
+
+      setTimeout(function(){
+        direction = -(direction);
+        for (let i = 0; i < direction; i++) {
+          let firstItem = carousel.querySelector('.carousel-item:first-child');
+          firstItem.parentNode.appendChild(firstItem);
+        }
+        carousel.classList.remove('transition');
+        carousel.style.transform = 'translateX(0px)';
+      },700);
+
+      carousel.querySelector('.carousel-item:nth-child(2)').classList.add('active');
+    }
+    return index;
   }
 })();
 
